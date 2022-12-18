c0b5dd38f22b3557582e309dd4ff727e0f1537b4
Amirhossein Ghasemzadeh <amirghasemzadeh17@gmail.com>
2022-12-18 13:57:59 +0330

    build 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-// import AddOrEditUser from './components/AddorEditUser/AddOrEditUser';
 import {Container} from '@mui/material';
-import Modal from './components/Modal';
-import UserTable from './components/UserTable/UserTable';
-import {UserContext} from './context/UserContext';
+
 const App = () => {
   return (
     <Container maxWidth='lg'>
-      <UserContext>
-        <Modal />
-        <UserTable />
-      </UserContext>
-      {/* <AddOrEditUser /> */}
+      <p>hello world</p>
     </Container>
   );
 };
